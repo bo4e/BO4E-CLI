783162981f770864cc2f1dc51b75abb0e472ae06
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2024-08-20 21:06:14 +0200

    .

--- src/bo4e_cli/__init__.py
+++ src/bo4e_cli/__init__.py
@@ -1,5 +1,6 @@
 """
 BO4E - Business Objects for Energy
+
 CLI for developers working with BO4E.
 For more information see 'bo4e --help' or visit [GitHub](https://github.com/bo4e/BO4E-CLI?tab=readme-ov-file#bo4e-cli).
 """
