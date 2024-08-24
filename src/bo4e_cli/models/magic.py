daedaaa17b2fc17e2e0d45bf8c1fa0ea3331f7e8
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2024-08-24 23:13:01 +0200

    🔥

--- src/bo4e_cli/models/magic.py
+++
@@ -1,5 +0,0 @@
-class MagicForwardMixin:
-    def __getattr__(self, item):
-        if item.startswith("__") and item.endswith("__") and self.__:
-            raise AttributeError(item)
-        return self[item]
