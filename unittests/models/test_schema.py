3d05d1d9428ef94293a8d6277cc63ce8ed7e799f
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2024-08-26 15:22:35 +0200

    MOOOREEE

--- unittests/models/test_schema.py
+++ unittests/models/test_schema.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from pydantic import TypeAdapter
 
-from bo4e_cli.models.schema import AnyOf, Decimal, Reference, SchemaRootObject, SchemaRootType
+from bo4e_cli.models.schema import AnyOf, Decimal, Reference, SchemaRootObject, SchemaRootStrEnum, SchemaRootType
 
 TEST_DIR = Path(__file__).parents[1] / "test_data/bo4e_rel_refs"
 
@@ -24,3 +24,10 @@ class TestSchema:
             schema = TypeAdapter(SchemaRootType).validate_json(f.read())
         assert isinstance(schema, SchemaRootObject)
         assert isinstance(schema.properties["zaehlerkonstante"].any_of[0], Decimal)
+
+    def test_enum(self):
+        with open(TEST_DIR / "enum/Sparte.json", "r") as f:
+            schema = TypeAdapter(SchemaRootType).validate_json(f.read())
+        assert isinstance(schema, SchemaRootStrEnum)
+        assert "STROM" in schema.enum
+        assert "GAS" in schema.enum
