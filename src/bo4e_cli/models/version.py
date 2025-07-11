b219717351270dd8dbd535eac3259afaa4b1ee0f
Leon Haffmans <49658102+lord-haffi@users.noreply.github.com>
2025-07-11 17:11:51 +0200

    feat: Adapt `Version` model to match the versions created by hatch in `BO4E-python` (#95)

--- src/bo4e_cli/models/version.py
+++ src/bo4e_cli/models/version.py
@@ -2,6 +2,7 @@
 Contains a model for BO4E versions.
 """
 
+import datetime
 import functools
 import re
 from typing import TYPE_CHECKING
@@ -13,7 +14,8 @@ REGEX_VERSION = re.compile(
     r"(?P<functional>\d+)\."
     r"(?P<technical>\d+)"
     r"(?:-rc(?P<candidate>\d*))?"
-    r"(?:\+dev(?P<commit>\w+))?$"
+    r"(?:\+g(?P<commit_part>\w+)"
+    r"(?:\.d(?P<dirty_workdir_date_year>\d{4})(?P<dirty_workdir_date_month>\d{2})(?P<dirty_workdir_date_day>\d{2}))?)?$"
 )
 
 
@@ -27,15 +29,21 @@ class Version(BaseModel):
     functional: int
     technical: int
     candidate: int | None = None
-    commit: str | None = None
+    commit_part: str | None = None
     """
-    The commit hash.
+    The commit hash or at least a starting substring of it.
     When retrieving the version from a commit which has no tag on it, the version will have the commit hash
     after the last version tag in the history.
     """
+    dirty_workdir_date: datetime.date | None = None
+    """
+    E.g. if you have local changes in your working directory of the BO4E-python repo, the generation of schemas
+    will create versions like ``v202401.0.1.dev0+g12984hdac.d20240101``.
+    The last part after the ``.d`` is the date of the last change in the working directory - I believe...
+    """
 
     @classmethod
-    def from_str(cls, version: str) -> "Version":
+    def from_str(cls, version: str, *, allow_dirty_version: bool = True) -> "Version":
         """
         Parse a version string into a Version object e.g. 'v202401.0.1-rc8+dev12asdf34' or 'v202401.0.1'.
         Raises a ValueError if the version string is invalid.
@@ -43,7 +51,22 @@ class Version(BaseModel):
         match = REGEX_VERSION.match(version)
         if match is None:
             raise ValueError(f"Invalid version: {version}")
-        return cls.model_validate(match.groupdict())
+        if not allow_dirty_version and match.group("dev_number") is not None:
+            raise ValueError(f"Dirty version not allowed: {version}")
+        field_dict = match.groupdict()
+        if field_dict["dirty_workdir_date_year"] is not None:
+            # If the dirty workdir date is present, parse it into a date object
+            field_dict["dirty_workdir_date"] = datetime.date(
+                year=int(field_dict.pop("dirty_workdir_date_year")),
+                month=int(field_dict.pop("dirty_workdir_date_month")),
+                day=int(field_dict.pop("dirty_workdir_date_day")),
+            )
+        else:
+            field_dict["dirty_workdir_date"] = None
+            del field_dict["dirty_workdir_date_year"]
+            del field_dict["dirty_workdir_date_month"]
+            del field_dict["dirty_workdir_date_day"]
+        return cls.model_validate(field_dict)
 
     @classmethod
     def is_valid(cls, version: str) -> bool:
@@ -61,9 +84,9 @@ class Version(BaseModel):
         """Check if the version is a release candidate."""
         return self.candidate is not None
 
-    def is_local_commit(self) -> bool:
-        """Check if the version is on a commit without a tag."""
-        return self.commit is not None
+    def is_dirty(self) -> bool:
+        """Check if the version is on a commit without a tag or corresponds to a dirty working directory."""
+        return self.commit_part is not None
 
     def __str__(self) -> str:
         return f"v{self.to_str_without_prefix()}"
@@ -73,8 +96,10 @@ class Version(BaseModel):
         version = f"{self.major}.{self.functional}.{self.technical}"
         if self.candidate is not None:
             version += f"-rc{self.candidate}"
-        if self.commit is not None:
-            version += f"+dev{self.commit}"
+        if self.is_dirty():
+            version += f"+g{self.commit_part}"
+            if self.dirty_workdir_date is not None:
+                version += f".d{self.dirty_workdir_date:%Y%m%d}"
         return version
 
     def __eq__(self, other: object) -> bool:
@@ -90,17 +115,18 @@ class Version(BaseModel):
         """
         if not isinstance(other, Version):
             return NotImplemented
-        if self.is_local_commit() and other.is_local_commit():
-            raise ValueError("Cannot compare two versions with local commit part.")
+        if self.is_dirty() and other.is_dirty():
+            raise ValueError("Cannot compare two dirty versions. Please rely on analysis with git commands instead.")
         for attr in ["major", "functional", "technical"]:
             if getattr(self, attr) != getattr(other, attr):
                 return getattr(self, attr) < getattr(other, attr)  # type: ignore[no-any-return]
         if self.candidate != other.candidate:
             return other.candidate is None or (self.candidate is not None and self.candidate < other.candidate)
-        if self.commit != other.commit:
-            return self.commit is None  # Implies other.commit is not None
-            # I.e. if the other version is at least one commit ahead to this version, it's considered as newer.
-        return False  # self == other
+        return other.is_dirty()
+        # If other version is dirty, the self version cannot be dirty due to the above check.
+        # In this case we consider the dirty version as newer.
+        # If the other version is not dirty, the self version is either dirty or not. In either case, the self version
+        # is not older than the other version.
 
     if TYPE_CHECKING:  # pragma: no cover
 
