c791f5990074658c311c55677e00a0a35d0cd8a2
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2025-07-11 21:31:11 +0200

    🩹Fix tests

--- unittests/cli/repo/test_versions.py
+++ unittests/cli/repo/test_versions.py
@@ -20,15 +20,13 @@ class TestLastVersions:
 
         version_tags = result.stdout.splitlines()
 
-        assert version_tags == ["v202401.7.1", "v202401.6.0", "v202401.5.0"]
+        assert version_tags == ["v202401.7.0", "v202401.6.0", "v202401.5.0"]
 
     def test_repo_last_versions_with_table(self) -> None:
         with change_cwd(BO4E_PYTHON_DIR):
             result = CliRunner().invoke(
                 app,
                 ["-v", "repo", "versions", "-sr", "ff94097932d6e2e0aadb515281f53f3619a93486"],
-                # This commit sha corresponds to the (wrongly tagged) 2025.0.0 tag.
-                # For some reason, dependabot created this tag?
                 catch_exceptions=False,
                 # terminal_width=20,
                 # It seems that the click runner constraints the terminal width + this option does not work.
@@ -41,7 +39,9 @@ class TestLastVersions:
         assert "Commit date" in result.stdout, f"Expected 'Commit date' in output, got: {result.stdout}"
 
         # Check if the output contains some expected versions
-        assert "v202401.7.1" in result.stdout, f"Expected 'v202401.7.1' in output, got: {result.stdout}"
+        assert (
+            "Skipping version v202401.7.1" in result.stdout
+        ), f"Expected 'v202401.7.1' in output, got: {result.stdout}"
         assert "v202401.7.0" in result.stdout
         assert "v202401.6.0" in result.stdout
         assert "v202401.5.0" in result.stdout
