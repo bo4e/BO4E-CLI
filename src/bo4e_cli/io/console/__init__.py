91c398bde5bed592c6a9cdc051671d4e06be3dc1
Leon Haffmans <leon.haffmans@hochfrequenz.de>
2025-08-19 10:20:31 +0200

    Delete Python project

--- src/bo4e_cli/io/console/__init__.py
+++
@@ -1,8 +0,0 @@
-"""
-This package provides a global Console instance that can be used to print messages to the console.
-"""
-
-from .console import CONSOLE
-from .monkey import *  # Load monkey patches
-
-__all__ = ["CONSOLE"]
